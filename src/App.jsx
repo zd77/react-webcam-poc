499703fb9162e8907217245c5e7a8dd44867979f
Dereck Z <zavaladereck77@gmail.com>
2022-12-17 12:16:40 -0600

    New camera service ( from scratch )

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,60 @@
+import { useRef, useState, useEffect } from 'react'
 import './App.css'
-import Camera, { FACING_MODES } from 'react-html5-camera-photo'
-import Webcam from 'react-webcam'
-import { Fragment } from 'react'
 
 function App() {
-  const handleCameraStart = (stream) => console.log('handleCameraStart')
+  const videoRef = useRef(null)
+  const photoRef = useRef(null)
+  const [hasPhoto, setHasPhoto] = useState(false)
+
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: { width: 1920, height: 1080 }})
+      .then( stream => {
+        let video = videoRef.current
+        video.srcObject = stream
+        video.play()
+      })
+      .catch( err => {
+        console.error(err)
+      })
+  }
+
+  const takePhoto = () => {
+    const width = 414
+    const height = width / (16/9)
+
+    let video = videoRef.current
+    let photo = photoRef.current
+
+    photo.width = width
+    photo.height = height
+
+    let context = photo.getContext('2d') 
+    context.drawImage( video, 0, 0, width, height )
+    setHasPhoto( true )
+  }
+
+  const closePhoto = () => {
+    let photo = photoRef.current
+    let context = photo.getContext('2d') 
+    context.clearRect( 0, 0, photo.width, photo.height)
+    setHasPhoto( false )
+  }
+
+  useEffect(() => {
+    getVideo()
+  }, [videoRef])
+  
   return (
-    <Fragment>
-      <div>react-webcam</div>
-      <Webcam 
-        audio={ false }
-        width={350}
-        height={350}
-      />
-      <div>react-html5</div>
-      <Camera 
-        isImageMirror={ true }
-        idealFacingMode={ FACING_MODES.USER}
-        idealResolution={{ width: 350, height: 350 }}
-        onCameraStart={ handleCameraStart }
-      />
-    </Fragment>
+    <div className="app">
+      <div className="camera">
+        <video ref={videoRef}></video>
+        <button onClick={takePhoto}>SNAP!</button>
+      </div>
+      <div className={"result " + (hasPhoto ? 'hasPhoto' : '')}>
+        <canvas ref={photoRef}></canvas>
+        <button onClick={closePhoto}>CLOSE!</button>
+      </div>
+    </div>
   )
 }
 
